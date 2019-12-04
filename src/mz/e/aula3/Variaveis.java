c26f02dd1a2a03417b9871cdf7e8c2e2b4199480
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-04 11:54:16 +0200

    Criação de variáveis

--- src/mz/e/aula3/Variaveis.java
+++ src/mz/e/aula3/Variaveis.java
@@ -4,7 +4,41 @@ package mz.e.aula3;
 /**
  *
  * @author joseseie
+ * @author Horcha Chambule
+ * @author Fenias Manhenge
+ * @author Iben Enky
+ * @author Nicolas Muchate
+ * @author Hassan Mutola
+ * @author Alfredo Francisco
+ * 
  */
 public class Variaveis {
+
+    public static void main (String [] args) {
+        
+        // Variáveis do tipo 'int'
+        int idade = 10; //inicializada
+        int horasDeEntrada;
+        
+         // Variáveis do tipo 'double'
+        double preco = 2500.50; //inicializada
+        double temperatura;
+        
+         // Variáveis do tipo 'char'
+        char sexo = 'M';
+        
+        
+        // Variáveis do tipo "String"
+        String sexoStr = "Masculino";
+        String texto = "Eu amo Java!";
+        
+        /*
+            TPC:
+            1. O que são variaveis em java
+            1. Pesquisar sobre os tipos de variáveis em Java.
+            2. Criar 5 Variaveis a sua escolha para cada tipo encontrado.
+        */
+        
+    }
     
 }
