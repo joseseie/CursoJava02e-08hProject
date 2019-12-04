732c789c12c2c147ab1813c798c0cffbae8d083f
Jose Seie <joseeugenioseie@hotmail.com>
2019-12-04 12:00:47 +0200

    Actualização dos autores das classes

--- src/mz/e/aula3/Operadores.java
+++ src/mz/e/aula3/Operadores.java
@@ -1,22 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mz.e.aula3;
 
 /**
  *
  * @author joseseie
+ * @author Horcha Chambule
+ * @author Fenias Manhenge
+ * @author Iben Enky
+ * @author Nicolas Muchate
+ * @author Hassan Mutola
+ * @author Alfredo Francisco
+ * 
  */
 public class Operadores {
     
     /*
-            TPC:
-            1. O que são operadores em java
-            1. Pesquisar sobre os tipos de operadores em Java.
-            2. Dar 10 exemplos com diferentes operadores.
-        */
+        TPC:
+        1. O que são operadores em java
+        1. Pesquisar sobre os tipos de operadores em Java.
+        2. Dar 10 exemplos com diferentes operadores.
+    */
         
     
 }
--- src/mz/e/aula3/TiposPrimitivos.java
+++ src/mz/e/aula3/TiposPrimitivos.java
@@ -1,13 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package mz.e.aula3;
 
 /**
  *
  * @author joseseie
+ * @author Horcha Chambule
+ * @author Fenias Manhenge
+ * @author Iben Enky
+ * @author Nicolas Muchate
+ * @author Hassan Mutola
+ * @author Alfredo Francisco
+ * 
  */
 public class TiposPrimitivos {
     
